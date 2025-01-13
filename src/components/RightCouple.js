05af05ce2fee9c4c4b6fca9429a1c5149a320bfa
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-01-13 23:30:09 +0700

    couple Section

--- src/components/RightCouple.js
+++ src/components/RightCouple.js
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 import "../assets/style/rightCouple.css"
-import womanPhoto from "../assets/image/leftImage8.jpg"
-import manPhoto from "../assets/image/leftImage9.jpg"
+import womanPhoto from "../assets/image/girlPhoto.jpg"
+import manPhoto from "../assets/image/boyPhoto.jpg"
+import coupleDiamond from "../assets/image/coupleDiamond.png"
 import namaMan from "../assets/image/namaMan.png"
 import namaWoman from "../assets/image/namaWoman.png"
 
@@ -12,15 +13,16 @@ const RightCouple = () => {
             <div className="coupleRight">
                 <div className="titleCoupleRight d-flex justify-content-center align-items-center">
                     <div>
-                        By The Grace of God
-                        <br />
-                        with the blessing of our parents
+                        By The Grace of God   with the blessing of our parents
                         <br />
                         We request the honour of your presence at the marriage of
                     </div>
                 </div>
 
                 <div className="manRight">
+                    <div className="textGroom d-flex justify-content-center align-items-center">
+                        The Groom
+                    </div>
                     <div className="manPhotoWrapper d-flex justify-content-center align-items-center">
                         <img src={manPhoto} className="manPhoto" />
                     </div>
@@ -29,19 +31,24 @@ const RightCouple = () => {
                         Fendy Santoso
                     </div>
 
-                    <div className="parentManRightWrapper mt-4 d-flex justify-content-center align-items-center">
+                    <div className="parentManRightWrapper d-flex justify-content-center align-items-center">
                         <div className="parentManRight">
                             <div>Anak Tercinta Dari</div>
-                            <div>Mr. Fam Djan Lim (Alm.) & Mrs. Bong Djan Lie</div>
+                            <div>Mr. Fam Djan Lim (✟) & Mrs. Bong Djan Lie</div>
                         </div>
                     </div>
                 </div>
 
-                <div className="rightAnd text-center">
-                    &
+                <div className="rightAnd text-center mb-4 d-flex justify-content-center align-items-center">
+                    <div className="leftBorder"></div>
+                    <img src={coupleDiamond} className="coupleRing" />
+                    <div className="leftBorder"></div>
                 </div>
 
                 <div className="womanRight">
+                    <div className="textBride d-flex justify-content-center align-items-center">
+                        The Bride
+                    </div>
                     <div className="womanPhotoWrapper d-flex justify-content-center align-items-center">
                         <img src={womanPhoto} className="manPhoto" />
                     </div>
@@ -50,10 +57,10 @@ const RightCouple = () => {
                         Bumi Ayu
                     </div>
 
-                    <div className="parentWomanRightWrapper mt-4 d-flex justify-content-center align-items-center">
+                    <div className="parentWomanRightWrapper d-flex justify-content-center align-items-center">
                         <div className="parentWomanRight">
                             <div>Anak Tercinta Dari</div>
-                            <div>Mr. Fam Djan Lim (Alm.) & Mrs. Bong Djan Lie</div>
+                            <div>Mr. Fam Djan Lim (✟) & Mrs. Bong Djan Lie</div>
                         </div>
                     </div>
                 </div>
