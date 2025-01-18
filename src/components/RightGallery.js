11de8d908d542b6073892c5aef05feb204988e15
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-01-19 01:07:40 +0700

    section right gallery

--- src/components/RightGallery.js
+++ src/components/RightGallery.js
@@ -1,17 +1,19 @@
 import React, { useRef, useEffect, useState } from "react";
-import "../assets/style/rightGallery.css"
-import leftImage2 from "../assets/image/leftImage2.jpg"
-import leftImage3 from "../assets/image/leftImage3.jpg"
-import leftImage4 from "../assets/image/leftImage4.jpg"
-import leftImage5 from "../assets/image/leftImage5.jpg"
-import leftImage6 from "../assets/image/leftImage6.jpg"
-import leftImage7 from "../assets/image/leftImage7.jpg"
-import leftImage8 from "../assets/image/leftImage8.jpg"
-import leftImage9 from "../assets/image/leftImage9.jpg"
+import "../assets/style/rightGallery.css";
+import leftImage1 from "../assets/image/gallery/img1.jpg";
+import leftImage2 from "../assets/image/gallery/img2.jpg";
+import leftImage3 from "../assets/image/gallery/img3.jpg";
+import leftImage4 from "../assets/image/gallery/img4.jpg";
+import leftImage5 from "../assets/image/gallery/img5.jpg";
+import leftImage6 from "../assets/image/gallery/img6.jpg";
+import leftImage7 from "../assets/image/gallery/img7.jpg";
+import leftImage8 from "../assets/image/gallery/img8.jpg";
+import leftImage9 from "../assets/image/gallery/img9.jpg";
+import leftImage10 from "../assets/image/gallery/img10.jpg";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 const RightGallery = () => {
-    
+
     return (
         <div className="wrapperRightGalleryAll">
             <div className="rightGalleryAll">
@@ -26,6 +28,9 @@ const RightGallery = () => {
                     showThumbs={true}
                     fade={true}
                 >
+                    <div className="imageCarrouselRight">
+                        <img src={leftImage1} />
+                    </div>
                     <div className="imageCarrouselRight">
                         <img src={leftImage2} />
                     </div>
@@ -49,6 +54,9 @@ const RightGallery = () => {
                     </div>
                     <div className="imageCarrouselRight">
                         <img src={leftImage9} />
+                    </div>
+                    <div className="imageCarrouselRight">
+                        <img src={leftImage10} />
                     </div>
                 </Carousel>
             </div>
